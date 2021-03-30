ddeee373162503d6e9540b92fd0881aeeca0f85c
Oscar <64810034+PandastudiosSWE@users.noreply.github.com>
2021-03-30 11:37:21 +0200

    Extendedmode!

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,7 +10,7 @@ version "2.4.0"
 server_scripts {
   "@async/async.lua",
   "@mysql-async/lib/MySQL.lua",
-  "@es_extended/locale.lua",
+  "@extendedmode/locale.lua",
   "locales/*.lua",
   "config.lua",
   "server/classes/c_trunk.lua",
@@ -19,7 +19,7 @@ server_scripts {
 }
 
 client_scripts {
-  "@es_extended/locale.lua",
+  "@extendedmode/locale.lua",
   "locales/*.lua",
   "config.lua",
   "client/esx_trunk-cl.lua"
